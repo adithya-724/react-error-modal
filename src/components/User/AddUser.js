6948293fb4125490e1f53104996f2c23f588c95f
adithya-724 <adithyamadhu2@gmail.com>
2022-01-15 16:16:34 +0530

    added state variables for user input and age

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = () => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const submitHandler = (event) => {
     event.preventDefault();
+    console.log(enteredAge, enteredName);
+  };
+  const nameHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const ageHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
   return (
     <Card>
       <form onSubmit={submitHandler} className={styles["form-container"]}>
         <div className={styles.item}>
           <label htmlFor="name">Username</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" onChange={nameHandler} />
         </div>
         <div className={styles.item}>
           <label htmlFor="age">Age</label>
-          <input type="number" id="age" />
+          <input type="number" id="age" onChange={ageHandler} />
         </div>
         <Button type="submit">Add User</Button>
       </form>
