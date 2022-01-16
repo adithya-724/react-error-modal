6989f97999df231b02307829a1bd240229c97e87
adithya-724 <adithyamadhu2@gmail.com>
2022-01-16 22:27:15 +0530

    added portals for backdrops

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,19 +1,40 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const Modal = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>{props.title}</header>
+      <p className={styles.content}>{props.content}</p>
+      <Button className={styles.center} onClick={props.onConfirm}>
+        Close
+      </Button>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={styles.backdrop} onClick={props.onConfirm} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>{props.title}</header>
-        <p className={styles.content}>{props.content}</p>
-        <Button className={styles.center} onClick={props.onConfirm}>
-          Close
-        </Button>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.querySelector("#backdrop-element")
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          content={props.content}
+          onConfirm={props.onConfirm}
+        />,
+        document.querySelector("#modal")
+      )}
     </div>
   );
 };
